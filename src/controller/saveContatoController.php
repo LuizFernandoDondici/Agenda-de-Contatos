5bf70eec0909faeb8122361ee4a378a8ca277d04
LuizFernandoDondici <luizfernando.dondici@outlook.com>
2022-05-30 15:36:12 -0300

    created: class contato

---
+++ src/controller/saveContatoController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Controller;
+
+use Controller\Interface\RequestController;
+
+class SaveContatoController implements RequestController
+{
+
+    public function __construct()
+    {
+    }
+
+    public function processRequest(): void
+    {
+        
+    }
+
+}
