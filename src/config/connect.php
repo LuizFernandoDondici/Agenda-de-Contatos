bd8f5df9fa335ae27f3330efcc46a2531a349d60
LuizFernandoDondici <luizfernando.dondici@outlook.com>
2022-05-30 14:57:07 -0300

    created: connection db

---
+++ src/config/connect.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Config;
+
+use PDO;
+
+class Connect
+{
+
+    public static function createConnection()
+    {
+        try {
+            
+            $pdo = new PDO ('sqlite:'. __DIR__ .'../../database/agenda-contatos.sqlite');
+
+            $query = 
+                'CREATE TABLE IF NOT EXISTS contatos(
+                    id INTEGER PRIMARY KEY,
+                    nome TEXT,
+                    telefone TEXT,
+                    celular TEXT,
+                    email TEXT,
+                    data_nascimento TEXT,
+                    cep TEXT,
+                    rua TEXT,
+                    numero TEXT,
+                    complemento TEXT,
+                    bairro TEXT,
+                    cidade TEXT,
+                    uf TEXT
+                );';
+
+            $pdo->exec($query);
+
+            return $pdo;
+
+        } catch (\Throwable $th) {
+            echo "erro DB";
+        }
+    }
+
+}
