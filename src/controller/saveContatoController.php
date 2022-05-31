17bc0f65fa39b1476d82c73b5d2ffba961365324
LuizFernandoDondici <luizfernando.dondici@outlook.com>
2022-05-31 08:41:55 -0300

    created: save contato

--- src/controller/saveContatoController.php
+++ src/controller/saveContatoController.php
@@ -3,6 +3,8 @@
 namespace AgendaDeContatos\Src\Controller;
 
 use AgendaDeContatos\Src\Controller\Interface\RequestController;
+use AgendaDeContatos\Src\Model\Contato;
+use AgendaDeContatos\Src\Model\ContatoDAO;
 
 class SaveContatoController implements RequestController
 {
@@ -14,6 +16,26 @@ class SaveContatoController implements RequestController
     public function processRequest(): void
     {
         
+        $contato = new Contato(
+            null, 
+            $_POST['name'], 
+            $_POST['tel'], 
+            $_POST['cel'], 
+            $_POST['email'], 
+            $_POST['birthday'], 
+            $_POST['cep'], 
+            $_POST['street'], 
+            $_POST['num'], 
+            $_POST['comp'], 
+            $_POST['district'], 
+            $_POST['city'],
+             $_POST['uf']
+        );
+
+        $contatoDAO = new ContatoDAO();
+
+        $contatoDAO->createContato($contato);
+
     }
 
 }
---
+++ src/model/contatoDAO.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace AgendaDeContatos\Src\Model;
+
+use AgendaDeContatos\Src\Config\Connect;
+
+class ContatoDAO
+{
+
+    private $conn;
+    
+
+    public function __construct()
+    {
+        $this->conn = Connect::createConnection();
+    }
+
+    
+    public function createContato(Contato $contato):bool
+    {
+        try {
+            
+            $insertQuery = 
+                'INSERT INTO contatos
+                (nome, telefone, celular, email, data_nascimento, 
+                cep, rua, numero, complemento, bairro, cidade, uf) 
+                VALUES 
+                (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
+
+            $stmt = $this->conn->prepare($insertQuery);
+
+            $stmt->bindParam(1, $contato->getName());
+            $stmt->bindParam(2, $contato->getTel());
+            $stmt->bindParam(3, $contato->getCel());
+            $stmt->bindParam(4, $contato->getEmail());
+            $stmt->bindParam(5, $contato->getBirthday());
+            $stmt->bindParam(6, $contato->getCep());
+            $stmt->bindParam(7, $contato->getStreet());
+            $stmt->bindParam(8, $contato->getNum());
+            $stmt->bindParam(9, $contato->getComp());
+            $stmt->bindParam(10, $contato->getDistrict());
+            $stmt->bindParam(11, $contato->getCity());
+            $stmt->bindParam(12, $contato->getUf());
+
+            return $stmt->execute();
+
+        } catch (\Throwable $th) {
+            echo 'erro insert';
+        }
+    }
+
+}
