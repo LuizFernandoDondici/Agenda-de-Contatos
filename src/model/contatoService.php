84fb6bbee7088fd5607dc2c546062d1e9f3ee1ef
LuizFernandoDondici <luizfernando.dondici@outlook.com>
2022-06-02 09:42:03 -0300

    fixed: contato service

--- src/model/contatoService.php
+++ src/model/contatoService.php
@@ -16,74 +16,74 @@ class ContatoService
         $msgError = '';
 
         if (!empty($contato->getName())) {
-            if (strlen($contato->getName()) > 50 || strlen($contato->getName()) < 3) {
-                $msgError = 'Erro ao salvar o nome';
+            if (strlen($contato->getName()) > 50) {
+                $msgError = 'Erro ao salvar o Nome.';
             }
         }
         
         if (!empty($contato->getTel())) {
             if (strlen($contato->getTel()) != 11) {
-                $msgError = 'Erro ao salvar o telefone';
+                $msgError = 'Telefone invalido.';
             }
         }
         
         if (!empty($contato->getCel())) {
             if (strlen($contato->getCel()) != 11) {
-                $msgError = 'Erro ao salvar o celular';
+                $msgError = 'Celular invalido.';
             }
         }
 
         if (!empty($contato->getEmail())) {
             if (strlen($contato->getEmail()) > 50 || !preg_match('/^[a-z0-9._-]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i', $contato->getEmail())) {
-                $msgError = 'Erro ao cadastrar o e-mail';
+                $msgError = 'E-mail invalido.';
             }
         }
        
         if (!empty($contato->getBirthday())) {
             if (strlen($contato->getBirthday()) != 10) {
-                $msgError = 'Erro ao cadastrar a data de nascimento';
+                $msgError = 'Data invalida.';
             }
         }
         
         if (!empty($contato->getCep())) {
             if (strlen($contato->getCep()) != 8) {
-                $msgError = 'Erro ao cadastrar o cep';
+                $msgError = 'Cep invalido.';
             }
         }
 
         if (!empty($contato->getStreet())) {
             if (strlen($contato->getStreet()) > 25) {
-                $msgError = 'Erro ao cadastrar a rua';
+                $msgError = 'Erro ao cadastrar a Rua.';
             }
         }
 
         if (!empty($contato->getNum())) {
             if (strlen($contato->getNum()) > 10) {
-                $msgError = 'Erro ao cadastrar o numero';
+                $msgError = 'Erro ao cadastrar o Numero.';
             }
         }
 
         if (!empty($contato->getComp())) {
             if (strlen($contato->getComp()) > 10) {
-                $msgError = 'Erro ao cadastrar o complemento';
+                $msgError = 'Erro ao cadastrar o Complemento.';
             }
         }
 
         if (!empty($contato->getDistrict())) {
             if (strlen($contato->getDistrict()) > 25) {
-                $msgError = 'Erro ao cadastrar o bairro';
+                $msgError = 'Erro ao cadastrar o Bairro.';
             }
         }
         
         if (!empty($contato->getCity())) {
             if (strlen($contato->getCity()) > 25) {
-                $msgError = 'Erro ao cadastrar a cidade';
+                $msgError = 'Erro ao cadastrar a Cidade.';
             }
         }
 
         if (!empty($contato->getUf())) {
-            if (strlen($contato->getUf()) != 2) {
-                $msgError = 'Erro ao cadastrar o uf';
+            if (!preg_match('/^(AC|AL|AP|AM|BA|CE|DF|ES|GO|MA|MT|MS|MG|PA|PB|PR|PE|PI|RJ|RN|RS|RO|RR|SC|SP|SE|TO)?$/i', $contato->getUf())) {
+                $msgError = 'UF invalido.';
             }
         }
         
